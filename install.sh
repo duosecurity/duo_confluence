7debd87212990128f7d1751898e1ab9bb2f7fe61
Peter Johnson <peter@duosecurity.com>
2013-07-15 17:41:35 -0400

    bump plugin version to 1.2

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ if [ -e $CONFLUENCE/confluence/WEB-INF/lib/duo.jar ]; then
 fi
 
 # make sure we haven't already installed
-if [ -e $CONFLUENCE/confluence/WEB-INF/lib/duo-filter-1.0-SNAPSHOT.jar ]; then
+if [ -e $CONFLUENCE/confluence/WEB-INF/lib/duo-filter-1.1-SNAPSHOT.jar ]; then
     echo "duo-filter-1.0-SNAPSHOT.jar already exists in $CONFLUENCE/confluence/WEB-INF/lib.  Move or remove this jar to continue."
     echo 'exiting'
     exit 1
@@ -77,7 +77,7 @@ if [ $? != 0 ]; then
 fi
 
 echo "duo_confluence jars have been installed. Next steps, in order:"
-echo "- Upload and install the plugin in etc/duo-twofactor-1.1-SNAPSHOT.jar "
+echo "- Upload and install the plugin in etc/duo-twofactor-1.2-SNAPSHOT.jar "
 echo "  using the Confluence web UI."
 echo "- Edit web.xml, located at $CONFLUENCE/confluence/WEB-INF/web.xml,"
 echo "  adding the following after the security filter and before any "
