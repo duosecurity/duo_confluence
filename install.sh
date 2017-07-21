9139f4d6ae3f4cf4dd4c5ae56c1753ff88aee3c2
Aaron McConnell <amcconnell@duosecurity.com>
2017-07-21 17:04:55 -0400

    Releasing version 1.4.3 of the Confluence plugin

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ AKEY=`python -c "import hashlib, os;  print hashlib.sha1(os.urandom(32)).hexdige
 # duo file variables
 DUO_WEB_FILENAME=DuoWeb-1.3.jar
 DUO_CLIENT_FILENAME=duo-client-0.2.1.jar
-DUO_FILTER_FILENAME=duo-filter-1.3.7.jar
-DUO_PLUGIN_FILENAME=duo-twofactor-1.4.2.jar
+DUO_FILTER_FILENAME=duo-filter-1.4.3.jar
+DUO_PLUGIN_FILENAME=duo-twofactor-1.4.3.jar
 
 usage () {
     printf >&2 "Usage: $0 [-d confluence directory] -i ikey -s skey -h host\n"
